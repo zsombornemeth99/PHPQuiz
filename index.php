7fdfeb3c71cd58886d4a8b3524246057d7798fcf
Leviii99 <nyiro.levente@gmail.com>
2020-04-18 13:21:57 +0200

    javítás

--- index.php
+++ index.php
@@ -47,14 +47,13 @@ if (isset($_POST["action"]) && $_POST["action"] == "cmd_insert") {
     !empty($_POST["input_c"]) &&
     !empty($_POST["input_d"]) &&
     !empty($_POST["input_helyes"])) {
-        $sql = "INSERT INTO quiz (kerdes, valasz_A, valasz_B, valasz_C, valasz_D, helyes) 
+        $sql = "INSERT quiz (kerdes, valasz_A, valasz_B, valasz_C, valasz_D, helyes) 
         VALUES ('".$_POST["input_kerdes"]."',
-                '".$_POST["input_a"]."',
-                '".$_POST["input_b"]."',
-                '".$_POST["input_c"]."',
-                '".$_POST["input_d"]."',
+                '".$_POST["input_a"]."'),
+                '".$_POST["input_b"]."'),
+                '".$_POST["input_c"]."'),
+                '".$_POST["input_d"]."'),
                 '".$_POST["input_helyes"]."')";
-                echo $sql;
         if (mysqli_query($conn, $sql)) {
             echo "Sikeres adatfelvétel!";
         } else {
@@ -68,27 +67,13 @@ if (isset($_POST["action"]) && $_POST["action"] == "cmd_insert") {
 
 /* FELVÉTEL VÉGE */
 
-//Törlés
-
-if (isset($_POST["action"]) && $_POST["action"] == "cmd_delete") 
-{
-    $sql="DELETE FROM quiz WHERE kerdes_id= ".$_POST['input_id'];
-    if (mysqli_query($conn, $sql)) {
-        echo "Sikeres törlés!";
-    } else {
-        echo "Sikertelen törlés!";
-    }
-}
-
-//Törlés vége
-
 /* LISTÁZÁS KEZDETE */
 
 $sql = "SELECT * FROM quiz";
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0)
 {
-    echo "<table><tr><th>Kérdés</th><th>A</th><th>B</th><th>C</th><th>D</th><th>Törlés</th></tr>";
+    echo "<table><tr><th>Kérdés</th><th>A</th><th>B</th><th>C</th><th>D</th></tr>";
     while($row = mysqli_fetch_assoc($result))
     {
         echo "<tr>".
@@ -97,147 +82,10 @@ if (mysqli_num_rows($result) > 0)
           "<td>".$row['valasz_B']."</td>".
           "<td>".$row['valasz_C']."</td>".
           "<td>".$row['valasz_D']."</td>".
-          "<td>"?> <form method="POST">
-          <input type="hidden" name="input_id" value="<?php echo $row['kerdes_id']; ?>">
-          <input type="hidden" name="action" value="cmd_delete">
-          <input type="submit" value="Törlés">
-          </form>
-          <?php
-          echo "</td>".
         "</tr>";   
     }   
     echo "</table>";
-}
-else
-{
-    echo "Nincs adat";
-}
-
-/* FELVÉTEL KEZDETE */
-
-
-/* FELVÉTEL VÉGE */
-
-
-/* LISTÁZÁS VÉGE */
-
-?>
-</body>
-</html><!DOCTYPE html>
-<html>
-<head>
-    <title>Quiz</title>
-    <style>
-        table, th, tr, td{
-            border: 1px solid black;
-            border-collapse: collapse;
-            text-align: center;
-        }
-        td, th {
-            width: 10%;
-            height: 50px;
-        }
-    </style>
-</head>
-<body>
-<form method="POST">
-    <input type="text" name="input_kerdes" placeholder="Kérdés"><br>
-    <input type="text" name="input_a" placeholder="'A' válasz"><br>
-    <input type="text" name="input_b" placeholder="'B' válasz"><br>
-    <input type="text" name="input_c" placeholder="'C' válasz"><br>
-    <input type="text" name="input_d" placeholder="'D' válasz"><br>
-    <input type="text" name="input_helyes" placeholder="Helyes válasz"><br>
-    <input type="hidden" name="action" value="cmd_insert">
-    <input type="submit" value="Felvétel">
-</form> <?php
-
-$servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "nemeth_zsombor";
-
-
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    mysqli_query($conn,"SET CHARACTER SET 'utf8'");
-
-/* FELVÉTEL  KEZDETE */
-
-if (isset($_POST["action"]) && $_POST["action"] == "cmd_insert") {
-    if (!empty($_POST["input_kerdes"]) &&
-    !empty($_POST["input_a"]) &&
-    !empty($_POST["input_b"]) &&
-    !empty($_POST["input_c"]) &&
-    !empty($_POST["input_d"]) &&
-    !empty($_POST["input_helyes"])) {
-        $sql = "INSERT INTO quiz (kerdes, valasz_A, valasz_B, valasz_C, valasz_D, helyes) 
-        VALUES ('".$_POST["input_kerdes"]."',
-                '".$_POST["input_a"]."',
-                '".$_POST["input_b"]."',
-                '".$_POST["input_c"]."',
-                '".$_POST["input_d"]."',
-                '".$_POST["input_helyes"]."')";
-                echo $sql;
-        if (mysqli_query($conn, $sql)) {
-            echo "Sikeres adatfelvétel!";
-        } else {
-            echo "Sikertelen adatfelvétel!";
-        }
-    }
-    else {
-        echo "Valami nincs kitöltve!";
-    }
-}
-
-/* FELVÉTEL VÉGE */
-
-//Törlés
-
-if (isset($_POST["action"]) && $_POST["action"] == "cmd_delete") 
-{
-    $sql="DELETE FROM quiz WHERE kerdes_id= ".$_POST['input_id'];
-    if (mysqli_query($conn, $sql)) {
-        echo "Sikeres törlés!";
-    } else {
-        echo "Sikertelen törlés!";
-    }
-}
-
-//Törlés vége
-
-/* LISTÁZÁS KEZDETE */
-
-$sql = "SELECT * FROM quiz";
-$result = mysqli_query($conn, $sql);
-if (mysqli_num_rows($result) > 0)
-{
-    echo "<table><tr><th>Kérdés</th><th>A</th><th>B</th><th>C</th><th>D</th><th>Törlés</th></tr>";
-    while($row = mysqli_fetch_assoc($result))
-    {
-        echo "<tr>"; ?>
-          <td><?php echo $row['kerdes']; ?></td>
-          <td id='<?php echo 'valaszA'.$row['kerdes_id']; ?>' style="cursor: pointer;" onclick="helyesE('<?php echo $row['valasz_A']; ?>', '<?php echo $row['helyes']; ?>', '<?php echo 'valaszA'.$row['kerdes_id']; ?>')"><?php echo $row['valasz_A']; ?></td>
-          <td id='<?php echo 'valaszB'.$row['kerdes_id']; ?>' style="cursor: pointer;" onclick="helyesE('<?php echo $row['valasz_B']; ?>', '<?php echo $row['helyes']; ?>', '<?php echo 'valaszB'.$row['kerdes_id']; ?>')"><?php echo $row['valasz_B']; ?></td>
-          <td id='<?php echo 'valaszC'.$row['kerdes_id']; ?>' style="cursor: pointer;" onclick="helyesE('<?php echo $row['valasz_C']; ?>', '<?php echo $row['helyes']; ?>', '<?php echo 'valaszC'.$row['kerdes_id']; ?>')"><?php echo $row['valasz_C']; ?></td>
-          <td id='<?php echo 'valaszD'.$row['kerdes_id']; ?>' style="cursor: pointer;" onclick="helyesE('<?php echo $row['valasz_D']; ?>', '<?php echo $row['helyes']; ?>', '<?php echo 'valaszD'.$row['kerdes_id']; ?>')"><?php echo $row['valasz_D']; ?></td>
-            <script>
-                function helyesE(valasz, helyesvalasz, id) {
-                    if (valasz == helyesvalasz) {
-                        document.getElementById(id).style.backgroundColor = "green";
-                        document.getElementById(id).style.color = "white";
-                    } else {
-                        document.getElementById(id).style.backgroundColor = "red";
-                        document.getElementById(id).style.color = "white";
-                    }
-                    setTimeout(function(){ document.getElementById(id).style.backgroundColor = "white"; }, 500);
-                    setTimeout(function(){ document.getElementById(id).style.color = "black"; }, 500);
-                }
-            </script> <?php  
-        echo "</tr>";
-    }   
-    echo "</table>";
+        
 }
 else
 {
