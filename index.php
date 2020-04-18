e179e141de4ad63271ce7deb8748de8942155554
zsombornemeth99 <zsombor.nemeth99@gmail.com>
2020-04-18 11:21:53 +0200

    kapcsolat létrehozása

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
 
+$servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "nemeth_zsombor";
+
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    mysqli_query($conn,"SET CHARACTER SET 'utf8'");
+
 /* FELVÉTEL KEZDETE */
+
 
 
 /* FELVÉTEL VÉGE */
